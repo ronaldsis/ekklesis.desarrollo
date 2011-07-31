ae4ecb4dd5640fa2084bedc3214cf923525c2bf0
luix <luix@siervo>
2011-07-31 13:58:31 -0530

    Configuracion de conexi?n a  BD Postgres

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,7 @@
 dataSource {
 	pooled = true
-	driverClassName = "org.hsqldb.jdbcDriver"
-	username = "sa"
-	password = ""
+	driverClassName = "org.postgresql.Driver"
+        dialect = org.hibernate.dialect.PostgreSQLDialect
 }
 hibernate {
     cache.use_second_level_cache=true
@@ -14,7 +13,9 @@ environments {
 	development {
 		dataSource {
 			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-			url = "jdbc:hsqldb:mem:devDB"
+			url = "jdbc:postgresql:ekklesis"
+                        username = "ekklesis"
+                        password = "ekklesis"
 		}
 	}
 	test {
